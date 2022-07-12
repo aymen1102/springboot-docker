ff79d77e236ec2d22e8e18e54dab23352f140a55
Aymen BAROUDI <40143957+aymen1102@users.noreply.github.com>
2022-07-12 16:45:54 +0200

    Update SpringbootDockerApplication.java

--- src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestAController
 public class SpringbootDockerApplication {
 
 	@GetMapping("/welcome")
